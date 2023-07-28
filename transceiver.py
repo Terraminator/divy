449cc50c41be3646f9c3e27e9812e8084245d6bb
Terraminator <terraminator@protonmail.com>
2023-07-28 22:44:56 +0200

    fix stuff

--- transceiver.py
+++ transceiver.py
@@ -8,7 +8,7 @@ ENTRY="11111111"
 EXIT="10000001"
 
 PACKET_SIZE=80 # 80bytes   later 1024bytes?
-HEADER_LENGTH=29 #29bytes
+HEADER_LENGTH=37 #37bytes
 PAYLOAD_SIZE=PACKET_SIZE-HEADER_LENGTH
 
 def str_to_bin(s):
@@ -37,33 +37,33 @@ def bin_to_int(b, pad_size=8):
     tmp=""
     for o in b:
         if len(tmp)==pad_size:
-            yield int(tmp, 2)
+            yield(int(tmp, 2))
             tmp=""
         tmp+=o
-    yield int(tmp, 2)
+    yield(int(tmp, 2))
 
 def bin_to_bytes(b, pad_size=8):
     tmp=""
     for o in b:
         if len(tmp)==pad_size:
-            yield int(tmp, 2).to_bytes(1, byteorder='big', signed=True) 
+            yield(int(tmp, 2).to_bytes(1, byteorder='big', signed=True)) 
             tmp=""
         tmp+=o
-    yield int(tmp, 2).to_bytes(1, byteorder='big', signed=True) 
+    yield(int(tmp, 2).to_bytes(1, byteorder='big', signed=True))
     
 
 def chunks(data, size):
     tmp=""
     for o in data:
         if len(tmp) == size:
-            yield tmp
+            yield(tmp)
             tmp=""
         else:
             tmp+=o
     yield tmp.zfill(size)
 
 def checksum(data, text=True):
-    return(sum(map(bin_to_int, data)))
+    return(sum(bin_to_int(data)))
 
 
 gpio.setmode(gpio.BCM)
@@ -93,7 +93,7 @@ class tx:
                 if seq==4096:
                     seq=1
                 for i in range(self.repeat):
-                    self.transmit_data(data, self.target, seq, flag)
+                    self.transmit_(data, self.target, seq, flag)
                 seq+=1
 
     def tbit_(self, sig):
@@ -104,28 +104,28 @@ class tx:
             gpio.output(self.tx_pin, 1)
             sleep(self.too)
 
-    def transmit_(self, data, target, seq, flag, header=False):
+    def transmit_(self, data, target, seq=None, flag=None, header=False):
         target=str_to_bin(target)
         if not header:
-            #header: 4bytes+8bytes+16bytes+1byte
+            #header: 4bytes+16bytes+16bytes+1byte
             #target: str_to_bin(zero/pi4b)
             flag=0 # is text/is frame
-            h = str_to_bin(target)+int_to_bin(seq, 8)+int_to_bin(checksum(data), 16)+int_to_bin(flag)
-            if len(h)!=HEADER_LENGTH*8: print("header length doesnt match constant!")
+            h = str_to_bin(target)+int_to_bin(seq, 16)+int_to_bin(checksum(data), 16)+int_to_bin(flag)
+            if len(h)!=HEADER_LENGTH*8: print("header length doesnt match constant: {}!={}".format(len(h), HEADER_LENGTH*8))
             for sig in ENTRY:
-                tbit_(sig)
-            self.transmit_data(h, target, header=True)
+                self.tbit_(sig)
+            self.transmit_(h, target, header=True)
         for sig in data:
-            tbit_(sig)
+            self.tbit_(sig)
         if not header:
             for sig in EXIT:
-                tbit_(sig)
+                self.tbit_(sig)
 
     def send(self, data, raw=False):
         if not raw:
             data=str_to_bin(data)
-        for c in chunks(data, PAYLOAD_SIZE):
-            if len(c)!=PAYLOAD_SIZE*8: print("payload size doesnt match constant!")
+        for c in chunks(data, PAYLOAD_SIZE*8):
+            if len(c)!=PAYLOAD_SIZE*8: print("payload size doesnt match constant: {}!={}".format(len(c), PAYLOAD_SIZE*8))
             self.q.put((c, raw))
 
     def cleanup(self):
@@ -141,11 +141,11 @@ class rx:
         self.repeat = repeat
         self.name=name #needed to read only packets addressed to this receiver
         self.q=Queue()
-        self.worker1=threading.Thread(target=fill_queue)
+        self.worker1=threading.Thread(target=self.fill_queue)
         self.worker1.start()
-        self.worker2=threading.Thread(target=watch_queue)
+        self.worker2=threading.Thread(target=self.watch_queue)
         self.worker2.start()
-        self.packets=q.Queue()
+        self.packets=Queue()
 
     def normalize(self, b):
         return(b[::2])
@@ -153,17 +153,17 @@ class rx:
     def fill_queue(self):
         b=""
         while True:
-            b+=gpio.input(self.rx_pin)
+            b+=str(gpio.input(self.rx_pin))
             sleep(self.delay)
-            if EXIT in normalize(b):
-                q.put(b[b.find(ENTRY)+8:b.find(EXIT)+8])
+            if EXIT in self.normalize(b):
+                self.q.put(b[b.find(ENTRY)+8:b.find(EXIT)+8])
                 b=b.replace(b[b.find(ENTRY)+8:b.find(EXIT)+8], "")
 
 
-    def get_header(packet):
+    def get_header(self, packet):
         try:
             h=packet[:HEADER_LENGTH*8]
-            header={"target":bin_to_str(h[:4*8]), "seq":bin_to_int(h[4*8:4*8+8*8]), "checksum":bin_to_int(h[4*8+8*8:4*8+8*8+16*8]), "flag":bin_to_int(h[4*8+8*8+16*8:4*8+8*8+16*8+8])}
+            header={"target":bin_to_str(h[:4*8]), "seq":bin_to_int(h[4*8:4*8+16*8]), "checksum":bin_to_int(h[4*8+16*8:4*8+16*8+16*8]), "flag":bin_to_int(h[4*8+16*8+16*8:4*8+16*8+16*8+8])}
         except:
             return(False)# header corrupted
         return(header)
@@ -189,10 +189,10 @@ class rx:
         tmp=[]
         max_p=self.repeat*2
         while True:
-            p=q.get()
-            seq=get_header(p)["seq"]
+            p=self.q.get()
+            seq=self.get_header(p)["seq"]
             if seq and seq not in seqs:
-                if get_payload(p, get_header(p)):
+                if self.get_payload(p, self.get_header(p)):
                     packets.put(p)
                     seqs.append(seq)
                     if seq==4095:
@@ -201,8 +201,8 @@ class rx:
 
     def recv(self):
         while True:
-            p=packets.get()
-            yield (get_header(p), get_header(p))
+            p=self.packets.get()
+            yield (self.get_header(p), self.get_header(p))
 
     def cleanup(self):
         del self.worker1
